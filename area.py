5e8060ff01812ee6fb3b06751f43925052e8d063
pragyaMCA17 <31646309+pragyaMCA17@users.noreply.github.com>
2017-09-05 07:03:48 +0000

    CHANGES1

---
+++ area.py
@@ -0,0 +1,23 @@
+def areaRectangle(length,breadth):
+    '''
+    Objective : to find the area of Rectangle
+    Input Paramters :
+        length  : length of rectangle
+        breadth : breadth of rectangle
+    Approach  : multiply length and breadth
+    Return Values : return area of rectangle
+    '''
+    area = length * breadth
+    return area
+    
+def areaTriangle(base,height):
+    '''
+    Objective : to find the area of Triangle
+    Input Paramters :
+        base   : base of triangle
+        height : height of triangle
+    Approach  : multiply base and height and then divide by 2
+    Return Values : return area of triangle
+    '''
+    area = (1/2) * base * height
+    return area
